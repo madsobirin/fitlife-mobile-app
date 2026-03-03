ec9a9b9130ba949db3faec4ada28c4fdea175006
kikinazifah <nazifakhitah@gmail.com>
2026-03-03 23:16:52 +0700

    artikel, kalkulator page

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // static const String baseUrl = "https://fitlife-web-platform.vercel.app/api";
-  static const String baseUrl = "http://192.168.1.6:8000/api";
+  static const String baseUrl = "http://192.168.100.84:8000/api";
 }
